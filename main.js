508bfcaa6c97a5c5d209bcadcbd058d9a85442c8
Morgan Neiman <quackhouse@me.com>
2014-01-08 11:28:37 -0500

    adds totally new layout

--- main.js
+++ main.js
@@ -1,33 +1,29 @@
-$(document).on("ajax:success", function(event, response){
-  alert("score");
-  // console.log(data, status, xhr);
-  $('body').append(response.message);
-});
-
-$(document).bind("ajaxSend", function(){
- $("#loading").show();
-}).bind("ajaxComplete", function(){
- $("#loading").hide();
-});
-
 $(document).ready(function(){
-
-  $('#repos').on("click", function(e){
-    // alert("clicked");
+  $('#proj').click(function(e){
     e.preventDefault();
-    $.ajax({
-      url: "https://api.github.com/users/quackhouse/repos",
-      type: "get",
-      dataType: "json"
-    }).done(function(data){
-      // alert("success");
-      console.log(data);
-      $.each(data, function(i){
-        var link = $('<a>').attr("href", data[i].html_url).text(data[i].name);
-        $('body').append(link);
-        $('body').append($('<br>'));
+    $('#dropdown').toggle(500);
+  });
+  $('#trix').click(function(e){
+    e.preventDefault();
+    $('#dropdown2').toggle(500);
+  });
+  if (!Modernizr.webgl) {
+    $('.webgl').css('display', 'none');
+  }
+  $('.trick').click(function(e){
+    e.preventDefault();
+    switch(this.id) {
+      case 'parallax':
+      $.getScript('skrollr.js', function(){
+        $('#nav').hide(200);
+        $('#cool-trick').text("Parallax & Scrolling Site");
+        $('body').css('background', 'none');
+        var s = skrollr.init();
+        $('#parallaxdiv').show();
 
       });
-    });
+      break;
+    }
+
   });
 });
